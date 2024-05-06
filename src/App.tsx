7a7ea9ffdafdd597c0cf6b6c8abb98b240af7883
Jasdeep2k15 <singhjasdeep1602@gmail.com>
2024-05-06 10:24:29 +0530

    worked on the filters

--- src/App.tsx
+++ src/App.tsx
@@ -24,39 +24,6 @@ function App() {
   console.log(FilteredData, 'in store');
   console.log(originalData, 'original');
 
-  // api call
-  useEffect(() => {
-    const myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-
-    const body = JSON.stringify({
-      limit: 20,
-      offset: 0,
-    });
-
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          'https://api.weekday.technology/adhoc/getSampleJdJSON',
-          {
-            method: 'POST',
-            headers: myHeaders,
-            body,
-          }
-        );
-        const result = await response.json();
-        const innerdata = result?.jdList || [];
-        // by default crowding the initial states
-        setOriginalData(innerdata);
-        dispatch(setFilteredData(innerdata));
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, [dispatch]);
-
   // filters on companyname,location,minexperience,minpay and role is done and no data on tech stack available in response
 
   // company name input and filter
@@ -89,7 +56,7 @@ function App() {
 
     // Check if value exists
     if (value) {
-      const experience = originalData?.filter(
+      const experience = FilteredData?.filter(
         (data: any) => data?.minExp === value
       );
       dispatch(setFilteredData(experience));
@@ -99,35 +66,168 @@ function App() {
     }
   };
 
+  // location input and filter
+
+  const [loc, setLoc] = useState<string>('');
+
+  const handleLocationChange = (event: SelectChangeEvent) => {
+    const value = event.target.value;
+    setLoc(value);
+
+    // Check if value exists
+    if (value) {
+      const location = FilteredData?.filter(
+        (data: any) => data?.location === value
+      );
+      dispatch(setFilteredData(location));
+    } else {
+      // If value is empty, dispatch the original array
+      dispatch(setFilteredData(originalData));
+    }
+  };
+
+  // role input and filter
+
+  const [role, setRole] = useState<string>('');
+
+  const handleRoleChange = (event: SelectChangeEvent) => {
+    const value = event.target.value;
+    setRole(value);
+
+    // Check if value exists
+    if (value) {
+      const yourRole = FilteredData?.filter(
+        (data: any) => data?.jobRole === value
+      );
+      dispatch(setFilteredData(yourRole));
+    } else {
+      // If value is empty, dispatch the original array
+      dispatch(setFilteredData(originalData));
+    }
+  };
+
+  // location input and filter
+
+  // const [loc, setLoc] = useState<string>('');
+
+  // const handleLocationChange = (event: SelectChangeEvent) => {
+  //   const value = event.target.value;
+  //   setLoc(value);
+
+  //   // Check if value exists
+  //   if (value) {
+  //     const location = FilteredData?.filter(
+  //       (data: any) => data?.location === value
+  //     );
+  //     dispatch(setFilteredData(location));
+  //   } else {
+  //     // If value is empty, dispatch the original array
+  //     dispatch(setFilteredData(originalData));
+  //   }
+  // };
+  //initial api call
+  useEffect(() => {
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+
+    const body = JSON.stringify({
+      limit: 20,
+      offset: 0,
+    });
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://api.weekday.technology/adhoc/getSampleJdJSON',
+          {
+            method: 'POST',
+            headers: myHeaders,
+            body,
+          }
+        );
+        const result = await response.json();
+        const innerdata = result?.jdList || [];
+        // by default crowding the initial states
+        setOriginalData(innerdata);
+        dispatch(setFilteredData(innerdata));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
+
   return (
     <div className='main'>
-      <input
-        className='input'
-        type='text'
-        id='basicInput'
-        value={inputValue}
-        onChange={handleInputChange}
-      />
+      <div className='fields'>
+        <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+          <InputLabel id='demo-select-small-label'>Exp</InputLabel>
+          <Select
+            labelId='demo-select-small-label'
+            id='demo-select-small'
+            value={minExp}
+            label='Exp'
+            onChange={handleExpChange}>
+            <MenuItem value=''>
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={2}>2</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={5}>5</MenuItem>
+            <MenuItem value={7}>7</MenuItem>
+            <MenuItem value={8}>8</MenuItem>
+          </Select>
+        </FormControl>
 
-      <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
-        <InputLabel id='demo-select-small-label'>Exp</InputLabel>
-        <Select
-          labelId='demo-select-small-label'
-          id='demo-select-small'
-          value={minExp}
-          label='Age'
-          onChange={handleExpChange}>
-          <MenuItem value=''>
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={1}>1</MenuItem>
-          <MenuItem value={2}>2</MenuItem>
-          <MenuItem value={3}>3</MenuItem>
-          <MenuItem value={5}>5</MenuItem>
-          <MenuItem value={7}>7</MenuItem>
-          <MenuItem value={8}>8</MenuItem>
-        </Select>
-      </FormControl>
+        <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+          <InputLabel id='demo-select-small-label'>location</InputLabel>
+          <Select
+            labelId='demo-select-small-label'
+            id='demo-select-small'
+            value={loc}
+            label='location'
+            onChange={handleLocationChange}>
+            <MenuItem value=''>
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value='delhi ncr'>Delhi ncr</MenuItem>
+            <MenuItem value='chennai'>Chennai</MenuItem>
+            <MenuItem value='bangalore'>banglore</MenuItem>
+            <MenuItem value='mumbai'>Mumbai</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+          <InputLabel id='demo-select-small-label'>Role</InputLabel>
+          <Select
+            labelId='demo-select-small-label'
+            id='demo-select-small'
+            value={role}
+            label='Role'
+            onChange={handleRoleChange}>
+            <MenuItem value=''>
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value='backend'>backend</MenuItem>
+            <MenuItem value='android'>android</MenuItem>
+            <MenuItem value='ios'>ios</MenuItem>
+            <MenuItem value='frontend'>frontend</MenuItem>
+            <MenuItem value='tech lead'>tech lead</MenuItem>
+          </Select>
+        </FormControl>
+
+        <input
+          className='input'
+          type='text'
+          id='basicInput'
+          placeholder='Company Name'
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      </div>
+
       <Grid
         container
         gap={5}
